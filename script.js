e7ccd8ee830328f898d2952636c489933dbd3e48
Kirans <kamsirichard1960@gmail.com>
2023-07-14 17:00:59 +0100

    Create script.js
    The javascript file

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputBox = document.getElementById('user');
+const pass = document.getElementById('password');
+
+function myFunction(){
+    if(inputBox.value === ''){
+        alert("Please put something in");
+    }
+    else{
+        alert("WELCOME")
+    }
+    
+}
+function myFunction(){
+    if(pass.value === ''){
+        alert("Please put something in");
+    }
+    else{
+        alert("WELCOME")
+    }
+    
+}
